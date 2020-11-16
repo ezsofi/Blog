feb4529628b12df52fd3a5c18f07808a2084e362
ezsofi <e.zsofiakovacs@gmail.com>
2020-11-16 14:44:21 +0100

    Removes posts

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -21,26 +21,44 @@ namespace Blog.Controllers
         [HttpGet("index")]
         public IActionResult Index()
         {
-            return View();
+            var posts = postService.GetAllPost();
+            return View(posts);
         }
 
         [HttpGet("post")]
-        public IActionResult Post()
+        public IActionResult Post(int id)
         {
-            return View();
+            var post = postService.GetPost(id);
+            return View(post);
         }
 
         [HttpGet("edit")]
-        public IActionResult Edit()
+        public IActionResult Edit(int? id)
         {
-            return View(new Post());
+            if(id == null)
+            {
+                return View(new Post());
+            }
+            else
+            {
+                var post = postService.GetPost((int) id);
+                return View(post);
+            }
+            
         }
 
         [HttpPost("edit")]
         public async Task<IActionResult> Edit(Post post)
         {
+            if(post.Id > 0)
+            {
+                postService.UpdatePost(post);
+            }
+            else
+            {
+                postService.AddPost(post);
+            }
 
-            postService.AddPost(post);
             if (await postService.SaveChangesAsync())
             {
                 return RedirectToAction("Index");
@@ -49,6 +67,15 @@ namespace Blog.Controllers
             {
                 return View(post);
             }
+
+        }
+        [HttpGet("remove")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            postService.RemovePost(id);
+            await postService.SaveChangesAsync();
+
+            return RedirectToAction("Index");
 
         }
     }
