e1c70bca165c147e431f12bd936853408c9bbb05
ezsofi <e.zsofiakovacs@gmail.com>
2020-11-16 10:13:45 +0100

    Render home and post pages

---
+++ Blog/Blog/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("post")]
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
